25c50a8f90d771d9f90483d5c70cc9b484faf5d7
Felipepotenza <potenza.fontes@gmail.com>
2023-04-30 00:21:36 -0400

    transport topic now working (I still need to do this for the rest of the topics). Make sure to change the uri path in qualifying.sdf

--- src/quali.py
+++ src/quali.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import Float64
 
 if __name__ == '__main__':
 
     rospy.init_node('thruster0')
-    pub = rospy.Publisher('/test', String, queue_size=50)
+    pub = rospy.Publisher('/model/clarke/joint/thruster0_joint/cmd_pos', Float64, queue_size=50)
     while True:
-        pub.publish("-30.0")
+        pub.publish(-30.0)
