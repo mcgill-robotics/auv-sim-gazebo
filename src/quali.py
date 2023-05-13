443c1715e149b11744896de2c6979ae7a45141e2
Felipepotenza <potenza.fontes@gmail.com>
2023-05-13 18:54:05 -0400

    mudanças no quali.py em relacao a bridge e setup pra testes no arquivo qualifying.launch

--- src/quali.py
+++ src/quali.py
@@ -2,7 +2,8 @@
 
 import rospy
 from std_msgs.msg import Float64
-from sensor_msgs.msg import Imu
+from sensor_msgs.msg import Imu, Image
+from geometry_msgs.msg import Wrench
 import time 
 import numpy as np
 from tf import transformations
@@ -26,19 +27,83 @@ from tf import transformations
 #             else:
 #                 self.euler[i] = angles[i]
 
+d = 0.224 #m
+D_1 = 0.895 #m
+D_2 = 0.778 #m
 
-def callback_imu(data):
-     pub_state_x.publish(data)
-     pub_state_y.publish(data)
-     pub_state_z.publish(data)
-     pub_state_theta_x.publish(data)
-     pub_state_theta_y.publish(data)
-     pub_state_theta_z.publish(data)
+MAX_FWD_FORCE = 4.52*9.81*0.5 
+MAX_BKWD_FORCE = -3.52*9.81*0.5 
+
+T = np.matrix(
+        [[1., 1., 0., 0., 0., 0., 0., 0.],
+        [0., 0., 1., -1., 0., 0., 0., 0.],
+        [0., 0., 0., 0., -1., -1., -1., -1.],
+        [0., 0., 0., 0., -d/2, d/2, d/2, -d/2],
+        [0., 0., 0., 0., -D_2/2, -D_2/2, D_2/2, D_2/2],
+        [-d/2, d/2, D_1/2, D_1/2, 0., 0., 0., 0.]]
+        )
+
+T_inv = np.linalg.pinv(T)
+
+
+def callback_thrusters(data):
+    a = np.array(
+            [[data.force.x],
+            [data.force.y],
+            [data.force.z],
+            [data.torque.x],
+            [data.torque.y],
+            [data.torque.z]]
+            )
+
+    converted_w = np.matmul(T_inv, a) 
+
+    pubt0.publish(converted_w[0])
+    pubt1.publish(converted_w[1])
+    pubt2.publish(converted_w[2])
+    pubt3.publish(converted_w[3])
+    pubt4.publish(converted_w[4])
+    pubt5.publish(converted_w[5])
+    pubt6.publish(converted_w[6])
+    pubt7.publish(converted_w[7])
+
+def callback_depth_cam(data):
+    pass
+
+def callback_down_cam(data):
+    pass
+
+def callback_imu_dvl(data):
+    p = data[0]
+    q = np.array([p.x, p.y, p.z, p.w])
+    theta_x = transformations.euler_from_quaternion(q, 'rxyz')[0]
+    theta_y = transformations.euler_from_quaternion(q, 'rxyz')[0]
+    theta_z = transformations.euler_from_quaternion(q, 'rxyz')[0]
+    pub_state_x.publish(p.x)
+    pub_state_y.publish(p.y)
+    pub_state_z.publish(p.z)
+    pub_state_theta_x.publish(theta_x)
+    pub_state_theta_y.publish(theta_y)
+    pub_state_theta_z.publish(theta_z)
 
 
 if __name__ == '__main__':
 
     rospy.init_node('thrusters')
+
+    # IMU + DVL
+    pub_state_x = rospy.Publisher('/state_x', Float64, queue_size=1)
+    pub_state_y = rospy.Publisher('/state_y', Float64, queue_size=1)
+    pub_state_z = rospy.Publisher('/state_z', Float64, queue_size=1)
+    pub_state_theta_x = rospy.Publisher('/state_theta_x', Float64, queue_size=1)
+    pub_state_theta_y = rospy.Publisher('/state_theta_y', Float64, queue_size=1)
+    pub_state_theta_z = rospy.Publisher('/state_theta_z', Float64, queue_size=1)
+
+    # pub_dvl = rospy.Publisher('', pass, queue_size=1)
+
+    sub_imu = rospy.Subscriber('/imu', Imu, callback_imu_dvl)
+
+    # Thrusters
     pubt0 = rospy.Publisher('/model/clarke/joint/thruster0_joint/cmd_pos', Float64, queue_size=1)
     pubt1 = rospy.Publisher('/model/clarke/joint/thruster1_joint/cmd_pos', Float64, queue_size=1) 
     pubt2 = rospy.Publisher('/model/clarke/joint/thruster2_joint/cmd_pos', Float64, queue_size=1)
@@ -47,16 +112,9 @@ if __name__ == '__main__':
     pubt5 = rospy.Publisher('/model/clarke/joint/thruster5_joint/cmd_pos', Float64, queue_size=1)
     pubt6 = rospy.Publisher('/model/clarke/joint/thruster6_joint/cmd_pos', Float64, queue_size=1)
     pubt7 = rospy.Publisher('/model/clarke/joint/thruster7_joint/cmd_pos', Float64, queue_size=1)
-    pub_state_x = rospy.Publisher('/state_x', Float64, queue_size=1)
-    pub_state_y = rospy.Publisher('/state_y', Float64, queue_size=1)
-    pub_state_z = rospy.Publisher('/state_z', Float64, queue_size=1)
-    pub_state_theta_x = rospy.Publisher('/state_theta_x', Float64, queue_size=1)
-    pub_state_theta_y = rospy.Publisher('/state_theta_y', Float64, queue_size=1)
-    pub_state_theta_z = rospy.Publisher('/state_theta_z', Float64, queue_size=1)
-
-    sub_imu = rospy.Subscriber('/imu', Imu, callback_imu)
-    # pub_depth_cam = rospy.Publisher('/[Front camera topic]', Sensor_msgs/msg/Image, queue_size=1)
-    # sub_depth_cam = rospy.Subscriber('/depth_cam', Sensor_msgs/msg/Image, queue_size=1)
+    
+    sub_effort = rospy.Subcriber('/effort', Wrench, callback_thrusters)
+    
     while True:
         pubt1.publish(50.0)
         pubt0.publish(50.0)
