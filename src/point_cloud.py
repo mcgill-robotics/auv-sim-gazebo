b97d58b8eda3ea3f5e81a097dd55758dbaa21726
Anthony Tan <anthony.tan2@mail.mcgill.ca>
2023-07-17 23:30:30 -0400

    works now

--- src/point_cloud.py
+++ src/point_cloud.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 
 import rospy
-from sensor_msgs.msg import PointCloud2, Image
+from sensor_msgs.msg import PointCloud2, Image, CameraInfo
 import numpy as np
 from sensor_msgs import point_cloud2
 from cv_bridge import CvBridge
 
+
+
 def camera_info_callback(msg):
-    global fx, fy, cx, cy, width, height, convert_map
+    global fx, fy, cx, cy, width, height, x_over_z_map, y_over_z_map, convert_map
     fx = msg.K[0]
     fy = msg.K[4]
     cx = msg.K[2]
@@ -24,23 +26,22 @@ def camera_info_callback(msg):
     convert_map = np.sqrt(1 + x_over_z_map**2 + y_over_z_map**2)
 
 def convert_from_uvd(msg):
-    if convert_map is None: return
-    time = rospy.Time(0)
-    d = bridge.imgmsg_to_cv2(msg)
-    # replace nan with inf
-    d = np.nan_to_num(d, nan=np.inf)
-    z_map = d / convert_map
-    # print amount of np.nan in x_over_z_map and y_over_z_map
-    x_map = x_over_z_map * z_map
-    y_map = y_over_z_map * z_map
+    if convert_map is not None:
+        time = rospy.Time(0)
+        d = bridge.imgmsg_to_cv2(msg)
+        # replace nan with inf
+        z_map = np.nan_to_num(d, nan=np.inf)
+        # print amount of np.nan in x_over_z_map and y_over_z_map
+        x_map = x_over_z_map * z_map
+        y_map = y_over_z_map * z_map
 
-    combined = np.stack((z_map, x_map, y_map), axis=2)
-
-    combined = combined.reshape((512*341, 3))
-    pub_msg = point_cloud2.create_cloud_xyz32(msg.header, combined)
-    pub_msg.header.frame_id = "auv_base"
-    pub_msg.header.stamp = time
-    point_cloud_pub.publish(pub_msg)
+        combined = np.stack((z_map, x_map, y_map), axis=2)
+        combined = combined.reshape((width*height, 3))
+        combined = combined.astype(np.float32)
+        pub_msg = point_cloud2.create_cloud_xyz32(msg.header, combined)
+        pub_msg.header.frame_id = "auv_base"
+        pub_msg.header.stamp = time
+        point_cloud_pub.publish(pub_msg)
 
 
 if __name__ == "__main__":
