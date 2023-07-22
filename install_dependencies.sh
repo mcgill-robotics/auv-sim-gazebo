415639a868f3cd831a0800bbe7f4934e9b792d29
antoinedang <antoine.dangeard@gmail.com>
2023-07-22 00:59:27 -0400

    fix unnecessary cmakelists definitions and update install dependencies script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -9,11 +9,10 @@ rosdep install -r --from-paths src -i -y --rosdistro noetic
 #ROS-IGN BRIDGE
 export IGNITION_VERSION=fortress
 auv_sim_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
-cd "${auv_sim_dir}/../../../"
+cd "${auv_sim_dir}/../"
 git clone https://github.com/mcgill-robotics/auv-ign
-cd auv-ign
+cd ..
 rosdep install -r --from-paths src -i -y --rosdistro noetic
 source /opt/ros/noetic/setup.bash
 # If rosdep fails to install Ignition libraries and you have not installed them before, please follow Ignition installation instructions:
 #  https://gazebosim.org/docs/latest/install
-catkin_make install
