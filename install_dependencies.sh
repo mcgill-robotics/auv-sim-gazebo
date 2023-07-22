32ea88ae13ac708eb7edd60b2dfb0bd99c07effe
antoinedang <antoine.dangeard@gmail.com>
2023-07-22 00:11:06 -0400

    change sim to build full ign package since it is not supported on all processors

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -5,10 +5,15 @@ sudo wget https://packages.osrfoundation.org/gazebo.gpg -O /usr/share/keyrings/p
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null
 sudo apt-get update
 sudo apt-get install -y ignition-fortress
-#ROS-IGN BRIDGE
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
-sudo apt-get update
-sudo apt install -y ros-noetic-ros-ign
-export IGNITION_VERSION=fortress
 rosdep install -r --from-paths src -i -y --rosdistro noetic
+#ROS-IGN BRIDGE
+export IGNITION_VERSION=fortress
+auv_sim_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+cd "${auv_sim_dir}/../../../"
+git clone https://github.com/mcgill-robotics/auv-ign
+cd auv-ign
+rosdep install -r --from-paths src -i -y --rosdistro noetic
+source /opt/ros/noetic/setup.bash
+# If rosdep fails to install Ignition libraries and you have not installed them before, please follow Ignition installation instructions:
+#  https://gazebosim.org/docs/latest/install
+catkin_make install
